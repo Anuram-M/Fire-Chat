a831e1a9ba31e1f0b5f2c82d259b121982595727
Anuram-M <anuram.mohan.85@gmail.com>
2026-05-16 14:21:09 +0530

    added app screenshots

--- app/src/main/java/com/ram/firechat/home/HomeComposable.kt
+++ app/src/main/java/com/ram/firechat/home/HomeComposable.kt
@@ -1,16 +1,16 @@
 package com.ram.firechat.home
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
@@ -18,9 +18,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -70,12 +70,18 @@ fun HomeComposable(navController: NavHostController, viewModel: FireViewModel) {
     }
     val colors = UXUtil.getColors()
     Scaffold(
-        modifier = Modifier.background(colors.primaryAccentColor).systemBarsPadding(),
+        modifier = Modifier
+            .background(colors.primaryAccentColor)
+            .systemBarsPadding(),
         topBar = {
-            Box(modifier = Modifier
-                .fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
                 Card(
-                    modifier = Modifier.fillMaxWidth().shadow(elevation = 10.dp, spotColor = Color.Black),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .shadow(elevation = 10.dp, spotColor = Color.Black),
                     shape = RoundedCornerShape(0.dp),
                     colors = CardDefaults.cardColors(
                         containerColor = colors.primaryAccentColor
@@ -102,124 +108,172 @@ fun HomeComposable(navController: NavHostController, viewModel: FireViewModel) {
                                 FireUtil.signOut(navController)
                             }
                         ) {
-                            Icon(painter = painterResource(R.drawable.logout), modifier = Modifier.size(28.dp), contentDescription = null)
+                            Icon(
+                                painter = painterResource(R.drawable.logout),
+                                modifier = Modifier.size(28.dp),
+                                contentDescription = null
+                            )
                         }
                     }
                 }
             }
         },
     ) { innerPadding ->
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-            .background(colors.primaryAccentColor)) {
-            Column(modifier = Modifier
+        Box(
+            modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 10.dp)) {
-            LazyRow {
-                items(users) { user ->
-                    val avatar = when {
-                        (user.avatarChoice == 1) -> R.drawable.satisfied256
-                        user.avatarChoice == 2 -> R.drawable.icecrystal256
-                        user.avatarChoice == 3 -> R.drawable.mask256
-                        else -> R.drawable.brightness256
-                    }
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Card(
-                            modifier = Modifier
-                                .size(100.dp)
-                                .clickable {
-                                    val chatid = listOf(FirebaseAuth.getInstance().currentUser?.uid.toString(), user.uid).sorted().joinToString("_")
-                                    viewModel.updateChatItems(user)
-                                    viewModel.setChatId(chatid)
-                                    navController.navigate("chat")
-                                }
-                                .padding(10.dp),
-                            shape = CircleShape
-                        ) {
-                            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                                Image(painter = painterResource(avatar), contentDescription = null, modifier = Modifier.size(80.dp), contentScale = ContentScale.Fit)
-                            }
-
-
+                .padding(innerPadding)
+                .background(colors.primaryAccentColor)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 10.dp)
+            ) {
+                LazyRow {
+                    items(users) { user ->
+                        val avatar = when {
+                            (user.avatarChoice == 1) -> R.drawable.satisfied256
+                            user.avatarChoice == 2 -> R.drawable.icecrystal256
+                            user.avatarChoice == 3 -> R.drawable.mask256
+                            else -> R.drawable.brightness256
                         }
-                        Text(
-                            text = user.userName,
-                            maxLines = 1,
-                            fontFamily = firaFamily,
-                            modifier = Modifier.width(80.dp),
-                            overflow = TextOverflow.Ellipsis,
-                            textAlign = TextAlign.Center
-                        )
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Card(
+                                modifier = Modifier
+                                    .size(100.dp)
+                                    .clickable {
+                                        val chatid = listOf(
+                                            FirebaseAuth.getInstance().currentUser?.uid.toString(),
+                                            user.uid
+                                        ).sorted().joinToString("_")
+                                        viewModel.updateChatItems(user)
+                                        viewModel.setChatId(chatid)
+                                        navController.navigate("chat")
+                                    }
+                                    .padding(10.dp),
+                                shape = CircleShape
+                            ) {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Image(
+                                        painter = painterResource(avatar),
+                                        contentDescription = null,
+                                        modifier = Modifier.size(80.dp),
+                                        contentScale = ContentScale.Fit
+                                    )
+                                }
+
+
+                            }
+                            Text(
+                                text = user.userName,
+                                maxLines = 1,
+                                fontFamily = firaFamily,
+                                modifier = Modifier.width(80.dp),
+                                overflow = TextOverflow.Ellipsis,
+                                textAlign = TextAlign.Center
+                            )
+                        }
                     }
                 }
-            }
 
-                if(quote != null) {
-                    Box(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+                if (quote != null) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    ) {
                         Box(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .background(shape = RoundedCornerShape(10.dp), color = Color.Black)
                                 .padding(10.dp)
-                                .height(120.dp), contentAlignment = Alignment.Center
+                            , contentAlignment = Alignment.Center
                         ) {
-                            Column {
+                            Column(
+                                modifier = Modifier.fillMaxWidth().padding(10.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
                                 Text(
                                     text = "Today's Quote",
                                     modifier = Modifier
                                         .fillMaxWidth()
                                         .padding(10.dp),
-                                    textAlign = TextAlign.Start,
+                                    textAlign = TextAlign.Center,
                                     color = colors.textColor,
                                     fontFamily = firaFamily,
                                     fontSize = 18.sp
                                 )
-                                Text(
-                                    text = "\" ${quote} \"",
-                                    fontSize = 16.sp,
-                                    modifier = Modifier.fillMaxWidth(),
-                                    textAlign = TextAlign.Center,
-                                    color = colors.textColor,
-                                    fontFamily = firaFamily,
-                                    fontWeight = FontWeight.Normal,
-                                    fontStyle = FontStyle.Italic
-                                )
+                                Row(
+                                    modifier = Modifier.scrollable(
+                                        rememberScrollState(),
+                                        orientation = Orientation.Horizontal
+                                    )
+                                ) {
+                                    Text(
+                                        text = "\" ${quote} \"",
+                                        fontSize = 16.sp,
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center,
+                                        color = colors.textColor,
+                                        fontFamily = firaFamily,
+                                        fontWeight = FontWeight.Normal,
+                                        fontStyle = FontStyle.Italic
+                                    )
+                                }
+
                             }
                         }
                     }
                 }
-                chats.isNotEmpty()
-                LazyColumn(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp))  {
+                if (chats.isNotEmpty())
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp)
+                    ) {
                         items(chats) { chat ->
-                            var avatar = when{
+                            var avatar = when {
                                 chat.avatarChoice == 1 -> R.drawable.satisfied256
                                 chat.avatarChoice == 2 -> R.drawable.icecrystal256
                                 chat.avatarChoice == 3 -> R.drawable.mask256
                                 else -> R.drawable.brightness256
                             }
-                            Row(modifier = Modifier
-                                .fillMaxSize()
-                                .padding(vertical = 5.dp)
-                                .clickable {
-                                    viewModel.updateChatItems(
-                                        UserModel(
-                                            userName = chat.userName,
-                                            avatarChoice = chat.avatarChoice,
-                                            uid = chat.receiverUserId,
-                                            fcmToken = ""
-                                        )
-                                    )
-                                    viewModel.setChatId(chatId = chat.chatId)
-                                    navController.navigate("chat")
-                                },
-                                verticalAlignment = Alignment.CenterVertically) {
-                                Image(painter = painterResource(avatar), contentScale = ContentScale.Fit , modifier = Modifier
-                                    .size(60.dp)
-                                    .background(Color.Black, shape = CircleShape), contentDescription = null )
-                                Column(modifier = Modifier
+                            Row(
+                                modifier = Modifier
                                     .fillMaxSize()
-                                    .padding(10.dp)) {
+                                    .padding(vertical = 5.dp)
+                                    .clickable {
+                                        viewModel.updateChatItems(
+                                            UserModel(
+                                                userName = chat.userName,
+                                                avatarChoice = chat.avatarChoice,
+                                                uid = chat.receiverUserId,
+                                                fcmToken = ""
+                                            )
+                                        )
+                                        viewModel.setChatId(chatId = chat.chatId)
+                                        navController.navigate("chat")
+                                    },
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Image(
+                                    painter = painterResource(avatar),
+                                    contentScale = ContentScale.Fit,
+                                    modifier = Modifier
+                                        .size(60.dp)
+                                        .background(Color.Black, shape = CircleShape),
+                                    contentDescription = null
+                                )
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(10.dp)
+                                ) {
                                     Text(
                                         text = chat.userName,
                                         fontSize = 15.sp,
@@ -239,9 +293,7 @@ fun HomeComposable(navController: NavHostController, viewModel: FireViewModel) {
 
                         }
                     }
-                    ?:
-
-                chats.isEmpty().let {
+                else
                     Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
                         Text(
                             modifier = Modifier.fillMaxWidth(),
@@ -249,16 +301,6 @@ fun HomeComposable(navController: NavHostController, viewModel: FireViewModel) {
                             text = "No Chats available!"
                         )
                     }
-                }
-
-                Button(onClick = {
-                    FireUtil.signOut(navController)
-                    Log.d("current", "HomeComposable: ${currentUser}")
-                }) {
-                    Text(
-                        text = "Home"
-                    )
-                }
             }
 
         }
